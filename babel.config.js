ca66323713681199a120438ef430eddfe3a34980
Aman <aman29271@gmail.com>
2019-10-29 03:15:40 +0530

    babel bundling updated

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: "current" },
+        useBuiltIns: "usage",
+        corejs: { version: 3, proposals: true }
+      }
+    ],
     "@babel/preset-react"
   ],
   ignore: ["**/*.css"]
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 // const nodeExternals = require("webpack-node-externals");
 module.exports = {
   entry: {
-    app: ["babel-polyfill","./src/browser/index.js"]
+    app: ["./src/browser/index.js"]
   },
   output: {
     path: path.resolve(__dirname, "public"),
