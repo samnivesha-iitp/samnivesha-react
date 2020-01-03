260a510de183a859a08887124568c2078e0a3cb4
Aman <aman29271@gmail.com>
2020-01-04 02:57:33 +0530

    modified webpack config

--- razzle.config.js
+++ razzle.config.js
@@ -2,6 +2,14 @@ const path = require("path");
 module.exports = {
   modify(config, { target, dev }, webpack) {
     const appConfig = config;
+    if (target === "node" && !dev) {
+      const serverEntry = path.resolve(__dirname, "src", "server");
+      appConfig.entry[0] = serverEntry;
+    }
+    if (target == "web" && !dev) {
+      const webEntry = path.resolve(__dirname, "src", "web", "client");
+      appConfig.entry.client = webEntry;
+    }
     if (target === "node" && dev) {
       const serverEntry = path.resolve(__dirname, "src", "server");
       appConfig.entry[2] = serverEntry;
@@ -10,6 +18,7 @@ module.exports = {
       const webEntry = path.resolve(__dirname, "src", "web", "client");
       appConfig.entry.client[1] = webEntry;
     }
+
     return appConfig;
   }
 };
